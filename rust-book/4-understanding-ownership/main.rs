69acaca5ad150baf28b8dbd8c34853847385c0cd
Agus Richard <agus.richard21@gmail.com>
2026-01-17 23:54:35 +0700

    4.2-references-and-borrowing

--- rust-book/4-understanding-ownership/main.rs
+++ rust-book/4-understanding-ownership/main.rs
@@ -1,10 +1,28 @@
 fn main() {
-    let first = String::from("Ferris");
-    let full = add_suffix(first);
-    println!("{full}");
+    // let s1 = String::from("Hello");
+    // let s2 = String::from("World");
+    // greet(&s1, &s2);
+    // println!("{} {}", s1, s2);
+
+    // let mut v: Vec<i32> = vec![1, 2, 3];
+    // let x: &i32 = &v[2];
+    // println!("{}", x);
+    // v.push(4);
+    // println!("{:?}", v);
+
+    // let mut v1: Vec<i32> = vec![1, 2, 3];
+    // let num: &mut i32 = &mut v1[2];
+
+    // println!("{}", num);
+    // *num += 1;
+    // println!("{:?}", v1);
+
+    // let mut v: Vec<i32> = vec![1, 2, 3];
+    // let num: &mut i32 = &mut v[2];
+    // println!("{:?}", v);
+    // println!("{}", num);
 }
 
-fn add_suffix(mut name: String) -> String {
-    name.push_str(" Jr.");
-    name
+fn greet(g1: &String, g2: &String) {
+    println!("{} {}", g1, g2);
 }
