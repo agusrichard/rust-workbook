f449ece437d6fd31a12d2bb583bf79f42c9530b0
Agus Richard <agus.richard21@gmail.com>
2026-01-19 02:01:25 +0700

    4.5-ownership-recap

--- rust-book/4-understanding-ownership/playground_4_5/src/main.rs
+++ rust-book/4-understanding-ownership/playground_4_5/src/main.rs
@@ -12,6 +12,19 @@ fn get_words(this: &Document) -> &[String] {
     this.as_slice()
 }
 
+fn get_first(strs: &mut (String, String)) -> &mut String {
+    &mut strs.0
+}
+fn get_second(strs: &mut (String, String)) -> &mut String {
+    &mut strs.1
+}
+// fn transfer_string(strs: &mut (String, String)) {
+//     let fst = get_first(strs);
+//     let snd = get_second(strs);
+//     fst.push_str(snd);
+//     snd.clear();
+// }
+
 fn main() {
     let words = vec!["hello".to_string()];
     let d = new_document(words);
@@ -21,4 +34,7 @@ fn main() {
     add_word(&mut d2, "world".to_string());
     println!("{:?}", d2);
     println!("{:?}", d);
+
+    // transfer_string(&mut ("hello".to_string(), "world".to_string()));
+    // println!("{:?}", ("hello".to_string(), "world".to_string()));
 }
