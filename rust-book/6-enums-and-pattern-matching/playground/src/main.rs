192b860bbc4e7cf5ff1f049cc417f06e6e686967
Agus Richard <agus.richard21@gmail.com>
2026-02-01 02:44:43 +0700

    6.3-if-let

--- rust-book/6-enums-and-pattern-matching/playground/src/main.rs
+++ rust-book/6-enums-and-pattern-matching/playground/src/main.rs
@@ -48,7 +48,34 @@ impl Message {
     }
 }
 
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+            // -- snip --
+        }
+    }
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new."))
+    }
+}
+
 fn main() {
     let m = Message::Move { x: 10, y: 10 };
-    m.call()
+    m.call();
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    }
 }
