b49066bbedc4fb9e43fd1ceba73e8365d938b1ea
Agus Richard <agus.richard21@gmail.com>
2026-02-01 02:18:01 +0700

    6.2-match

--- rust-book/6-enums-and-pattern-matching/playground/src/main.rs
+++ rust-book/6-enums-and-pattern-matching/playground/src/main.rs
@@ -6,6 +6,42 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // ....
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(x) => Some(x + 1),
+        None => None,
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            match state {
+                UsState::Alabama => 25,
+                UsState::Alaska => 25,
+            }
+        }
+    }
+}
+
 impl Message {
     fn call(&self) {
         println!("Something is here {:?}", self);
