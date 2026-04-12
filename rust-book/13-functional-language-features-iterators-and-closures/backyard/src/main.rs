cbc9f1f5078b0eeb9d812c37e6ce2cec51a5c8ce
Agus Richard <agus.richard21@gmail.com>
2026-04-12 23:36:58 +0700

    closure

--- rust-book/13-functional-language-features-iterators-and-closures/backyard/src/main.rs
+++ rust-book/13-functional-language-features-iterators-and-closures/backyard/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
     let add_one = |x: u32| x+1;
     println!("{}", add_one(1));
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
 }
