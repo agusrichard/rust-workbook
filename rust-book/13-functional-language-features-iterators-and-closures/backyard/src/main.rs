8f671e895c9dd442dcc468692f58dccc10a5abb2
Agus Richard <agus.richard21@gmail.com>
2026-04-13 23:19:28 +0700

    code 13.1

--- rust-book/13-functional-language-features-iterators-and-closures/backyard/src/main.rs
+++ rust-book/13-functional-language-features-iterators-and-closures/backyard/src/main.rs
@@ -1,8 +1,61 @@
-fn main() {
-    let add_one = |x: u32| x+1;
-    println!("{}", add_one(1));
+use std::thread;
 
-    let list = vec![1, 2, 3];
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn inventory() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+
+}
+
+fn immutable_borrowing() {
+    let list = vec![1,2,3];
     println!("Before defining closure: {list:?}");
 
     let only_borrows = || println!("From closure: {list:?}");
@@ -10,4 +63,32 @@ fn main() {
     println!("Before calling closure: {list:?}");
     only_borrows();
     println!("After calling closure: {list:?}");
+}
+
+fn mutable_borrowing() {
+    let mut list = vec![1,2,3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || list.push(4);
+
+    // println!("Error if the line is active: {list:?}");
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+}
+
+fn taking_ownership() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
+}
+
+
+fn main() {
+    inventory();
+    immutable_borrowing();
+    mutable_borrowing();
+    taking_ownership();
 }
