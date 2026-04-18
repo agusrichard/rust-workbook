660a6ab10316a67426fb06c011c0f46166cc2e54
Agus Richard <agus.richard21@gmail.com>
2026-04-18 23:37:59 +0700

    closure

--- rust-book/15-smart-pointers/backyard/src/main.rs
+++ rust-book/15-smart-pointers/backyard/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     let x = 5;
     let y = Box::new(x);
+    let s = String::from("hello");
 
     println!("{x}");
     println!("{y}");
+
+    let f = move || println!("{x}");
+    let g = move || println!("{s}");
+    f();
+    g();
+    println!("{x}");
+    // println!("{s}");
 
 }
