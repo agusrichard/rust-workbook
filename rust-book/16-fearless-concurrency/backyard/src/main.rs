7592b1fa5d9fb110f2687cbff3fb393353081498
Agus Richard <agus.richard21@gmail.com>
2026-05-06 23:28:08 +0700

    feat: chapter 16.2 - Using Message Passing to Transfer Data Between Threads

--- rust-book/16-fearless-concurrency/backyard/src/main.rs
+++ rust-book/16-fearless-concurrency/backyard/src/main.rs
@@ -1,7 +1,9 @@
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+fn threads() {
     let handler1 = thread::spawn(|| {
         for i in 1..10 {
             println!("number {i} from the spawned thread 1");
@@ -27,4 +29,51 @@ fn main() {
     handler1.join().unwrap();
     let handler2_result = handler2.join().unwrap();
     println!("handler1_result: {handler2_result}");
+
+}
+
+fn single_iterable_producer() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        for i in 1..5 {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for i in rx {
+        println!("{i}");
+    }
+}
+
+fn producer(tx: Sender<i32>, start: i32, end: i32) {
+    thread::spawn(move || {
+        for i in start..end {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+}
+
+fn consumer(rx: Receiver<i32>) {
+    for i in rx {
+        println!("Received: {i}");
+    }
+}
+
+fn multiple_consumers() {
+    let (tx1, rx) = mpsc::channel();
+
+    let tx2 = tx1.clone();
+
+    producer(tx1, 0, 5);
+    producer(tx2, 5, 10);
+    consumer(rx);
+}
+
+fn main() {
+    // threads();
+    // single_iterable_producer();
+    multiple_consumers();
 }
