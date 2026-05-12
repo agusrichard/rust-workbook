704da51576b35990b602b111e6339a82d55f5bdd
Agus Richard <agus.richard21@gmail.com>
2026-05-13 01:01:33 +0700

    code for chapter 17.2

--- rust-book/17-async-and-await/backyard/src/main.rs
+++ rust-book/17-async-and-await/backyard/src/main.rs
@@ -1,30 +1,11 @@
-use trpl::{Either, Html};
+pub mod futures_syntax;
+pub mod concurrency_with_async;
+
+use futures_syntax::run as fs_run;
+use concurrency_with_async::run as cwa_run;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    trpl::block_on(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
-
-        let (url, maybe_title) =
-            match trpl::select(title_fut_1, title_fut_2).await {
-                Either::Left(left) => left,
-                Either::Right(right) => right,
-            };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title was: '{title}'"),
-            None => println!("It had no title."),
-        }
-    })
+    // fs_run();
+    cwa_run();
 }
 
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let response_text = trpl::get(url).await.text().await;
-    let title = Html::parse(&response_text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
-}
