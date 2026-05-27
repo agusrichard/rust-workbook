c522aef6ef2da724a00ecf768d0cf5bb6103e3ff
Agus Richard <agus.richard21@gmail.com>
2026-05-27 18:38:06 +0700

    chapter 20.3: advanced types

---
+++ rust-book/20-advanced-features/backyard/src/chap_20_3.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);  // works — they're the same type
+}
--- rust-book/20-advanced-features/backyard/src/main.rs
+++ rust-book/20-advanced-features/backyard/src/main.rs
@@ -1,6 +1,8 @@
 pub mod chap_20_2;
+pub mod chap_20_3;
 
 use chap_20_2::run as run_20_2;
+use chap_20_2::run as run_20_3;
 
 fn main() {
     run_20_2();
